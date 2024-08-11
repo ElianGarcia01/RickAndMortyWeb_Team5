e4c9378d73594611dfc8c455ccad01adf4f631e4
Elian Garcia <telljua90@gmail.com>
2024-08-11 00:31:23 -0600

    Se crearon las tarjetas de personajes con vue en la pagina Characters

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,40 +1,89 @@
-import * as modulo from '../JavaScript/Modules.js'
+// import * as modulo from '../JavaScript/Modules.js'
 
-// SELECCION DE ELEMENTOS EN EL DOM
-let contenedorTarjetas = document.getElementById("contenedorTarjetas")
-let containerChecks = document.getElementById("containerChecks")
-let buscarTexto = document.getElementById('buscarTexto')
+// // SELECCION DE ELEMENTOS EN EL DOM
+// let contenedorTarjetas = document.getElementById("contenedorTarjetas")
+// let containerChecks = document.getElementById("containerChecks")
+// let buscarTexto = document.getElementById('buscarTexto')
 
-// Variables Universales
-let data
-let eventos
-let propiedadesUnicas
+// // Variables Universales
+// let data
+// let eventos
+// let propiedadesUnicas
 
-// Funcion para obtener informacion de la API
-function cargarDatos() {
-  modulo.obtenerDatos()
-    .then(datos => {
-      data = datos
-      eventos = data.results
-      console.log(eventos)
-      
-      propiedadesUnicas = [...new Set(eventos.map(event => event.category))]
+// // Funcion para obtener informacion de la API
+// function cargarDatos() {
+//   modulo.obtenerDatos()
+//     .then(datos => {
+//       data = datos
+//       eventos = data.results
+//       console.log(eventos)
 
-      // Importar funciones pintarTarjetas y checkboxs
-      modulo.pintarTarjetas(eventos, contenedorTarjetas)
-      modulo.pintarCheckboxs(propiedadesUnicas, containerChecks)
-    })
-    .catch(error => console.error('Error al cargar los datos:', error))
-}
+//       propiedadesUnicas = [...new Set(eventos.map(event => event.category))]
 
-// Funcion para cargar la funcion importadas
-function actualizarFiltros() {
-  let texto = buscarTexto.value.toLowerCase()
-  let checkboxesMarcados = Array.from(document.querySelectorAll('#containerChecks input[type=checkbox]')).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-  modulo.filtrarEventos(eventos, texto, checkboxesMarcados, contenedorTarjetas);
-}
+//       // Importar funciones pintarTarjetas y checkboxs
+//       modulo.pintarTarjetas(eventos, contenedorTarjetas)
+//       modulo.pintarCheckboxs(propiedadesUnicas, containerChecks)
+//     })
+//     .catch(error => console.error('Error al cargar los datos:', error))
+// }
 
-// EVENTOS
-window.addEventListener('load', cargarDatos)
-buscarTexto.addEventListener('input', actualizarFiltros)
-containerChecks.addEventListener('change', actualizarFiltros)
+// // Funcion para cargar la funcion importadas
+// function actualizarFiltros() {
+//   let texto = buscarTexto.value.toLowerCase()
+//   let checkboxesMarcados = Array.from(document.querySelectorAll('#containerChecks input[type=checkbox]')).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+//   modulo.filtrarEventos(eventos, texto, checkboxesMarcados, contenedorTarjetas);
+// }
+
+// // EVENTOS
+// window.addEventListener('load', cargarDatos)
+// buscarTexto.addEventListener('input', actualizarFiltros)
+// containerChecks.addEventListener('change', actualizarFiltros)
+
+// API Rick And Morty
+let urlApi = "https://rickandmortyapi.com/api/character"
+
+// Crear una constante para llamar una funcion
+const { createApp } = Vue
+
+// createApp es una funcion que recibe un objeto
+const app = createApp({
+  data() {
+    return {
+      personajes: [],
+      personajesCopy: [],
+      categorias: [],
+      textoBuscador: '',
+      categoriaSelec: [],
+    }
+
+  },
+  created() {
+    this.obtenerDatos(urlApi)
+
+  },
+  methods: {
+    obtenerDatos(url) {
+      fetch(url).then(response => response.json()).then(data => {
+
+        this.personajesCopy = data.results.filter(personaje => !personaje.status.includes('unknown'))
+        this.personajes = data.results.filter(personaje => !personaje.status.includes('unknown'))
+      })
+
+    }
+
+  },
+  computed: {
+    // FILTRO PARA PAGINA HOME
+    CharactersFilter() {
+      // Filtro de texto
+      let filtroTexto = this.personajesCopy.filter(personaje => personaje.name.toLowerCase().includes(this.textoBuscador.toLowerCase()))
+
+      // Verificar si el arreglo de categorias seleccionadas se encuentre vacio o no
+      if (this.categoriaSelec.length == 0) {
+        this.personajes = filtroTexto
+      } else {
+        this.personajes = filtroTexto.filter(personaje => personaje.category.includes(this.categoriaSelec))
+      }
+    }
+  }
+}).mount('#page')  //Montar en el createApp en el contenedor main
