572a9accd1a82c9da839166d804efb111b517df0
BuriticaSebas <jhoseburitica40@gmail.com>
2024-08-13 15:59:43 -0500

    cors metido

--- JavaScript/information.js
+++ JavaScript/information.js
@@ -1,22 +1,8 @@
 async function consumirApis() {
     try{
-
-        const personajes = await fetch('https://rickandmortyapi.com/api/character?page=40')
-        const lugares =  await fetch('https://rickandmortyapi.com/api/location')
-        const episodios =  await fetch('https://rickandmortyapi.com/api/episode')
-    
-        const convertirPersonajes = await personajes.json()
-        const obtenerPersonajes = convertirPersonajes.results
-        console.log( obtenerPersonajes)
-
-        const convertirLugares = await lugares.json()
-        const obtenerLugares = convertirLugares.results
-        console.log("Estos son los lugares: " + obtenerLugares)
-
-        const convertirEpisodios = await episodios.json()
-        const obtenerEpisodios = convertirEpisodios.results
-        console.log("Estos son los episodios: " + obtenerEpisodios)
-
+        const miapi = await fetch('https://api-creada.vercel.app/api/students')
+        const data = await miapi.json()
+        console.log(data)
 
 
     }catch(error){
