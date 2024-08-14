c09f1bb6ccb66d935988400263d9e4ff9692e622
Elian Garcia <telljua90@gmail.com>
2024-08-14 00:42:58 -0600

    pagina details

--- JavaScript/details.js
+++ JavaScript/details.js
@@ -1,27 +1,25 @@
 const { createApp } = Vue;
 
-    createApp({
-      data() {
-        return {
-          character: null,
-        };
-      },
-      created() {
-        this.fetchCharacterDetails()
-      },
-      methods: {
-        fetchCharacterDetails() {
-          const urlParams = new URLSearchParams(window.location.search);
-          const characterId = urlParams.get('id');
-
-          if (characterId) {
-            fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
-              .then(response => response.json())
-              .then(data => {
-                this.character = data;
-              })
-              .catch(error => console.error('Error fetching character details:', error));
-          }
-        }
-      }
-    }).mount('#detailsPage')
+const app = createApp({
+  data() {
+    return {
+      character: null,
+    };
+  },
+  created() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const characterId = urlParams.get('id');
+    this.fetchCharacterDetails(characterId);
+  },
+  methods: {
+    fetchCharacterDetails(id) {
+      const url = `https://rickandmortyapi.com/api/character/${id}`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          this.character = data;
+        })
+        .catch(error => console.error('Error fetching character details:', error));
+    },
+  },
+}).mount('#detailsApp');
