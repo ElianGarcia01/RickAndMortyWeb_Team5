1437fc309ed5b0df4574368459953fbed46b05bb
BuriticaSebas <jhoseburitica40@gmail.com>
2024-08-14 02:28:33 -0500

    Temporadas listas

--- JavaScript/information.js
+++ JavaScript/information.js
@@ -1,32 +1,34 @@
 async function consumirApis() {
-    try{
-       const miapi = await fetch("https://api-creada.vercel.app/api/sebastian")
-       const data = await miapi.json()
-
-       console.log(data)
-
-        const premios = data.premios
-        console.log(premios)
-
-        premios.forEach(element => {
-            console.log(element.año)
-        });
-
-        premios
-
-    const temporadas = data.temporadas
-    console.log(temporadas)
+    try {
 
 
-    temporadas.forEach(element => {
-        console.log(element.primeraEmision)
-    });
+        //Aca se consume la api general 
+        const apisebas = await fetch("https://api-creada.vercel.app/api/sebastian")
+        const data = await apisebas.json()
+        console.log(data)
 
-    const episodios = temporadas[0].primeraEmision
-    console.log(episodios)
+        //A partir de aca se guardara la parte de la API que necesito para el carrusel
+
+        //Comenzamos con temporadad y se la enviamos a la funcion pintar temporadas
+        const datosTemporadas = data.temporadas
+        pintarContendedorTemporada(datosTemporadas);
 
 
-    }catch(error){
+
+
+        //Seguimos con los premios que gano la serie 
+        const datosPremios = data.premios
+        pintarContenedorPremios(datosPremios)
+
+
+
+        //Por ultimo cogemos la informacion de voces 
+        const datosVoces = data.personajesDoblados
+        pintarContenedorVoces(datosVoces)
+
+        //
+
+    } catch (error) {
         console.log(error)
     }
 }
@@ -34,3 +36,97 @@ async function consumirApis() {
 
 consumirApis()
 
+
+
+const acordeonContenedor = document.getElementById('accordionFlushExample')
+function pintarContendedorTemporada(data) {
+    console.log(data)
+    data.forEach(element => {
+        acordeonContenedor.innerHTML += `
+                                         <div class="accordion-item">
+                                            <h2 class="accordion-header">
+                                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${element.acordeon}" aria-expanded="false" aria-controls="flush-collapse${element.acordeon}">
+                                                TEMPORADA ${element.numero}
+                                              </button>
+                                            </h2>
+                                            <div id="flush-collapse${element.acordeon}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                              <div class="accordion-body">
+                                                <div class="row mb-2">
+                                                    <div class="col">
+                                                        <p><strong>Cantidad de Episodios emitidos :</strong> ${element.episodios}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="row mb-2">
+                                                    <div class="col">
+                                                        <p><strong>Primera Emisión de la Temporada:</strong> ${element.primeraEmision}</p>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <p><strong>Última Emisión de la Temporada:</strong> ${element.ultimaEmision}</p>
+                                                    </div>
+                                                </div>        
+                                              </div>
+                                            </div>
+                                        </div>
+                                        
+
+    `;
+    });
+
+
+
+
+}
+
+
+
+function pintarContenedorPremios(data) {
+
+
+}
+
+
+
+function pintarContenedorVoces(data) {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const miapi = await fetch("https://api-creada.vercel.app/api/sebastian")
+      const data = await miapi.json()
+
+      console.log(data)
+
+       const premios = data.premios
+       console.log(premios)
+
+       premios.forEach(element => {
+           console.log(element.año)
+       });
+
+       premios
+
+   const temporadas = data.temporadas
+   console.log(temporadas)
+
+
+   temporadas.forEach(element => {
+       console.log(element.primeraEmision)
+   });
+
+   const episodios = temporadas[0].primeraEmision
+   console.log(episodios)*/
