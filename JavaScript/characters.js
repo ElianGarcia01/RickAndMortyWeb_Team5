61075aac494ad91c31a2c67bcf5bcae7311ee01a
Elian Garcia <telljua90@gmail.com>
2024-08-15 13:41:32 -0600

    seccion de favoritos

--- JavaScript/characters.js
+++ JavaScript/characters.js
@@ -4,53 +4,59 @@ const app = createApp({
   data() {
     return {
       personajes: [],
+      favoritos: [],
       filters: {
-        name: localStorage.getItem('filterName') || '',
-        gender: localStorage.getItem('filterGender') || '',
-        status: localStorage.getItem('filterStatus') || '',
-        species: localStorage.getItem('filterSpecies') || ''
+        name: '',
+        gender: '',
+        status: '',
+        species: ''
       },
-      currentPage: parseInt(localStorage.getItem('currentPage')) || 1,
+      currentPage: 1,
       totalPages: 1,
-      maxPagesToShow: 10
+      maxPagesToShow: 10 // Número máximo de páginas para mostrar en la paginación
     };
   },
   created() {
-    this.fetchCharacters(this.currentPage);
+    this.fetchCharacters()
+    let datosLocal = JSON.parse(localStorage.getItem('favoritos'))
+    if(datosLocal){
+      this.favoritos = datosLocal
+    }
   },
   methods: {
     fetchCharacters(page = 1) {
-      const url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+      const url = `https://rickandmortyapi.com/api/character/?page=${page}`
       fetch(url)
         .then(response => response.json())
         .then(data => {
-          this.personajes = data.results;
-          this.totalPages = data.info.pages;
-          this.currentPage = page;
-          localStorage.setItem('currentPage', page);
+          this.personajes = data.results
+          this.totalPages = data.info.pages
+          this.currentPage = page
         })
-        .catch(error => console.error('Error al llamar los datos:', error));
+        .catch(error => console.error('Error al llamar los datos:', error))
     },
     changePage(page) {
       if (page > 0 && page <= this.totalPages) {
-        this.fetchCharacters(page);
+        this.fetchCharacters(page)
       }
     },
-    saveFilters() {
-      localStorage.setItem('filterName', this.filters.name);
-      localStorage.setItem('filterGender', this.filters.gender);
-      localStorage.setItem('filterStatus', this.filters.status);
-      localStorage.setItem('filterSpecies', this.filters.species);
+    agregarFavoritos(personaje) {
+      if (!this.favoritos.includes(personaje)) {
+        this.favoritos.push(personaje)
+        localStorage.setItem('favoritos',JSON.stringify(this.favoritos))
+      }
+    },
+    quitarFavoritos(personaje) {
+      const index = this.favoritos.findIndex(fav => fav.id === personaje.id);
+      if (index !== -1) {
+        this.favoritos.splice(index, 1);
+        localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
+      }
     }
+    
   },
-  watch: {
-    filters: {
-      handler() {
-        this.saveFilters();
-      },
-      deep: true
-    }
-  },
+
+
   computed: {
     filteredCharacters() {
       return this.personajes.filter(character => {
@@ -59,18 +65,84 @@ const app = createApp({
           (this.filters.gender === '' || character.gender === this.filters.gender) &&
           (this.filters.status === '' || character.status === this.filters.status) &&
           (this.filters.species === '' || character.species === this.filters.species)
-        );
-      });
+        )
+      })
     },
+    // FUNCION PARA MOSTRAR SOLO DIEZ PAGINAS EN LA PAGINACION DE RESULTADOS
     pagesToShow() {
-      const pages = [];
-      const startPage = Math.max(1, this.currentPage - Math.floor(this.maxPagesToShow / 2));
-      const endPage = Math.min(this.totalPages, startPage + this.maxPagesToShow - 1);
+      const pages = []
+      const startPage = Math.max(1, this.currentPage - Math.floor(this.maxPagesToShow / 2))
+      const endPage = Math.min(this.totalPages, startPage + this.maxPagesToShow - 1)
 
       for (let i = startPage; i <= endPage; i++) {
-        pages.push(i);
+        pages.push(i)
       }
-      return pages;
+      return pages
     }
   }
-}).mount('#page');
+}).mount('#page')
+
+let lastScrollTop = 0; // Variable para guardar la última posición de scroll
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scroll hacia abajo
+    navbar.style.top = '-100px'; // Ajusta este valor al alto de tu navbar
+  } else {
+    // Scroll hacia arriba
+    navbar.style.top = '0';
+  }
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Actualiza la última posición de scroll
+}, false);
+
+document.addEventListener('DOMContentLoaded', function () {
+  const header = document.getElementById('header'); // Obtiene el header por su ID
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) { // Número de píxeles de scroll antes de cambiar el estilo
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const images = [
+    './Recursos/wallpaper.jpg',
+    './Recursos/wall2.jpg',
+    './Recursos/wall3.jpg',
+    './Recursos/wall4.jpg',
+  ];
+
+  const slideTrack = document.getElementById('slide-track');
+
+  // Función para crear y añadir imágenes al slider
+  function populateSlider(images, repeatTimes) {
+    for (let i = 0; i < repeatTimes; i++) {
+      images.forEach(src => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.alt = '';
+        img.classList.add('slider');
+        slideTrack.appendChild(img);
+      });
+    }
+  }
+
+  // Llama a la función con las imágenes y el número de veces que quieres repetirlas
+  populateSlider(images, 10); // Ajusta el número de repeticiones según sea necesario
+})
+
+const abrirModal = document.querySelector('#abrirModal')
+const cerrarModal = document.querySelector('#cerrarModal')
+const modal = document.querySelector('#modal')
+abrirModal.addEventListener("click", () => {
+  modal.showModal();
+})
+cerrarModal.addEventListener("click", () => {
+  modal.close();
+})
